0ed006bc751898190d07c2ceb97472d98ef8c44e
Joey-Einerhand <j.einerhand@live.nl>
2020-10-27 15:27:14 +0100

    Removed index file after experiment

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+var githubGet = jQuery;
+$(document).ready(function(){
+    var x = document.getElementById("test");
+    x.innerHTML = "Hello!";
+    $.get("https://api.github.com/users/Joey-Einerhand")
+    });
+
